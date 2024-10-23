5ae11a85cc19ea6c5403f324c9ce39e8ca80e907
blandoncj <jacoboblandon94@gmail.com>
2024-10-22 20:16:25 -0500

    refactor: remove UserDTO as it's no longer needed

--- app/dto/user_dto.py
+++
@@ -1,8 +0,0 @@
-class UserDTO:
-    def __init__(self, name: str, nickname: str, password: str):
-        self.name = name
-        self.nickname = nickname
-        self.password = password
-
-    def __str__(self):
-        return f"UserDTO(name={self.name}, nickname={self.nickname}, password={self.password})"
